5a7d829498c391fff7f0e8568e968b8bc36bbf05
AdrianoHorta <adriano_horta@hotmail.com>
2019-05-13 17:33:43 +0100

    Devolução e Rececao e Inserir Paletes

--- InserirPaletess.php
+++ InserirPaletess.php
@@ -2,6 +2,8 @@
 <html lang=pt dir="ltr">
 <?php
 //include 'operador.php';
+
+include 'navbarOperador.php';
 include 'db.php';
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
   if (isset($_POST['Confirm'])) {
@@ -132,35 +134,9 @@ elseif (isset($_POST['Guia_ID2'])) {
 </head>
 
 <body >
-    <nav role="navigation">
-        <ul class="nav nav-tabs">
-            <li class="nav-item">
-                <a class="nav-link" href="operador.php">Home</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="Guia_Operador_operador.php">Guia do Operador</a></li>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="showGuiaEntrega.php">Registar Palete</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="mudarpass_operador.php">Mudar Palavra-Passe</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="listagem_pedidos_armazem_operador.php">Pedidos</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link active" href="Guia_Rececao.php">Imprimir Receção</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="Guia_Devolucao.php">Imprimir Devolução</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="index.php">Sair</a>
-            </li>
-        </ul>
+
     </nav>
-    <form class="container" action="paletesTeste.php" method="post" id="mainForm" novalidate>
+    <form class="container" action="inserirPaletess.php" method="post" id="mainForm" novalidate>
     
     <div class="container" id="onLoad" >
         <div class="card card-container" style="text-align:center; width:120%; margin-right:auto; margin-left:auto; max-width: 100000px">
