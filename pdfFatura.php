9ee44f858ea6d9b308f72534fcacff6030ee7145
AdrianoHorta <40461050+AdrianoHorta@users.noreply.github.com>
2019-05-22 17:26:02 +0100

    Add files via upload

--- pdfFatura.php
+++ pdfFatura.php
@@ -7,11 +7,13 @@ $dbname = "estagio";
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 mysqli_set_charset($conn, "utf8");
+
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 require "fpdf.php";
+
 class myPDF extends FPDF
 {
     function header()
@@ -21,9 +23,11 @@ class myPDF extends FPDF
         $username = "root";
         $password = "";
         $dbname = "estagio";
+
         // Create connection
         $conn = new mysqli($servername, $username, $password, $dbname);
         mysqli_set_charset($conn, "utf8");
+
         // Check connection
         if ($conn->connect_error) {
             die("Connection failed: " . $conn->connect_error);
@@ -38,6 +42,7 @@ class myPDF extends FPDF
         date_default_timezone_set("Europe/Lisbon");
         $FinalDay = date("Y-m-t");
         $FirstDay = date("Y-m-01");
+
         $this->SetFont('Times', '', 12);
         $teste = $this->w;
         $teste = $teste / 2;
@@ -76,6 +81,7 @@ class myPDF extends FPDF
         $FinalDay = date("Y-m-t H:i:s");
         $FirstDay = date("Y-m-01 H:i:s");
     
+
         $query = mysqli_query($conn, "SELECT guia.data_prevista as guiadata,cliente.id as cliente_id, artigo.referencia as artigoreferencia, guia.id as id,artigo.id as artigo_id,guia.numero_paletes as numero_paletes, guia.data_prevista as data_prevista, guia.numero_requisicao as numero_requisicao, tipo_guia.nome as tgn ,guia.tipo_guia_id as tpg, zona.id as zona, zona.preco_zona as precozona, armazem.id as armazemid, armazem.custo_carga as acg, armazem.custo_descarga as asd FROM guia INNER JOIN cliente on guia.cliente_id = cliente.id INNER JOIN artigo on guia.artigo_id=artigo.id INNER JOIN armazem on guia.armazem_id=armazem.id INNER JOIN tipo_guia on tipo_guia.id=guia.tipo_guia_id INNER JOIN zona ON (zona.armazem_id=guia.armazem_id and zona.tipo_palete_id=guia.tipo_palete_id ) WHERE guia.cliente_id='" . $_POST['GetCliente'] . "' and(tipo_guia_id=4 or tipo_guia_id=3) and date(data_prevista) BETWEEN '" . $_POST['GetDataInicial'] . "' and '" . $_POST['GetDataFinal'] . "'");
         foreach ($query as $eachRow) {
             $CargaFinal = 0;
@@ -90,14 +96,18 @@ class myPDF extends FPDF
             $custoDescarga = $eachRow['asd'];
             $dataGuia = $eachRow['guiadata'];
             //OLA;
+
             $guiaid = $eachRow['id'];
             $ArtigoRef = $eachRow['artigoreferencia'];
+
             $result = $conn->query("SELECT count(*) FROM guia WHERE tipo_guia_id=3 AND cliente_id='$clienteId'");
             $row = $result->fetch_row();
             $count = $row[0];
+
             $result = $conn->query("SELECT count(*) FROM guia WHERE tipo_guia_id=4 AND cliente_id='$clienteId'");
             $row = $result->fetch_row();
             $count2 = $row[0];
+
             if ($tipoGuia == 3) {
                 $CargaFinal = $custoCarga * $count;
                 $tipoLinha = 1;
@@ -106,9 +116,12 @@ class myPDF extends FPDF
                 $tipoLinha = 2;
             }
             $queryPalete = mysqli_query($conn, "SELECT Data,Data_Saida FROM palete WHERE artigo_id='$ArtigoIDD'");
+
+
             foreach ($queryPalete as $eachRowPalete) {
                 $dataDescarga = $eachRowPalete['Data_Saida'];
                 $dataCarga = $eachRowPalete['Data'];
+
                 if ($dataDescarga == 0) {
                     $datetime1 = new DateTime($timeRN);
                     $datetime3 = $timeRN;
@@ -132,6 +145,7 @@ class myPDF extends FPDF
             $count = $row[0];
             if ($count == 0) {
                 $sql = "INSERT INTO linha (cliente_id, tipo_linha_id, guia_id, artigo_id,quantidade,valor,data_guia) VALUES ('" . $_POST['GetCliente'] . "',$tipoLinha,$guiaid, $ArtigoIDD ,$numPaletes,'$Total','$dataGuias')";
+
                 if (mysqli_query($conn, $sql)) { }
             }
             $this->Cell(30, 10, "$NomeGuia", 'B,L', 0, 'C');
@@ -158,15 +172,18 @@ class myPDF extends FPDF
     function viewTableBot($conn)
     {
         $this->SetFont('Times', 'B', 12);
+
         $res = $conn->query("SELECT sum(valor) as suma FROM linha WHERE cliente_id='" . $_POST['GetCliente'] . "' and data_guia BETWEEN '" . $_POST['GetDataInicial'] . "' and '" . $_POST['GetDataFinal'] . "' ");
         $val = $res->fetch_array();
         $tech_total = $val['suma'];
         $iva = $tech_total * 0.23;
         $iva = number_format($iva, 2, '.', '');
+
         date_default_timezone_set("Europe/Lisbon");
         $timeRN = date("Y-m-d H:i:s");
         $FinalDay = date("Y-m-t");
         $FirstDay = date("Y-m-01");
+
         $res2 = $conn->query("SELECT sum(quantidade) as sumpal FROM linha WHERE cliente_id='" . $_POST['GetCliente'] . "' and data_guia BETWEEN '" . $_POST['GetDataInicial'] . "' and '" . $_POST['GetDataFinal'] . "' ");
         $val2 = $res2->fetch_array();
         $totalpal = $val2['sumpal'];
@@ -176,15 +193,18 @@ class myPDF extends FPDF
         $this->Cell(25, 10, $iva . chr(128), 'B', 0, 'C');
         $this->Cell(35, 10, $tech_total . chr(128), 'B,R', 0, 'C');
         $this->Ln();
+
         $result = $conn->query("SELECT count(*) FROM documento  WHERE cliente_id = '" . $_POST['GetCliente'] . "' AND data_inicio='" . $_POST['GetDataInicial'] . "' AND data_fim='" . $_POST['GetDataFinal'] . "'");
         $row = $result->fetch_row();
         $count = $row[0];
         if ($count == 0) {
             $sql2 = "INSERT INTO documento (cliente_id, utilizador_id, data_emissao, data_inicio,data_fim,iva,total) VALUES ('" . $_POST['GetCliente'] . "','" . $_SESSION['perfilId'] . "','$timeRN','$FirstDay','$FinalDay','$iva','$tech_total')";
+
             if (mysqli_query($conn, $sql2)) {
                 $id = mysqli_insert_id($conn);
             }
         }
+
         $pdf = new myPDF();
         $pdf->AliasNbPages();
         $pdf->AddPage('L', 'A4', 0);
